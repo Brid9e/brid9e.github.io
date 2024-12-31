b4a0efe87ab5067f80e92e2bae2e1eae5f6324a6
王栋桥 <wangdongqiao1997@163.com>
2024-12-31 11:33:17 +0800

    refactor: 多页面滚动

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,10 +1,29 @@
 import ThreeModel from '../ThreeModel'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { useScroll } from 'ahooks'
 import styles from './index.module.scss'
+import { useRef } from 'react'
 
 function Container() {
+  const container = useRef(null)
+
+  const scroll = useScroll(container)
+
+  useGSAP(
+    (context, contextSafe) => {
+
+    },
+    { scope: container, dependencies: [scroll?.top], revertOnUpdate: true }
+  )
+
   return (
-    <main className={styles.content}>
-      <ThreeModel />
+    <main ref={container} className={styles.content}>
+      {[1, 2, 3, 4, 5, 6, 7].map((key) => (
+        <div key={key} className={`${styles['page-item']} item-gsap`}>
+          <ThreeModel />
+        </div>
+      ))}
     </main>
   )
 }
